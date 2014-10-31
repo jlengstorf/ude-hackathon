a175e40d4cc0f31207ccf73a8716d3e05db263a7
Andrew Fribush <andrewfribush@Andrews-MacBook-Pro.local>
2014-10-30 21:29:00 -0400

    Template added

--- src/app/js/controllers/products.js
+++ src/app/js/controllers/products.js
@@ -17,7 +17,17 @@ angular.module('udeHackathon')
             bestbuy_id: 1235,
             autodesk_file: '/path/to/file'
           }
-        };
+        },
+        attiphone = {
+      "name": "Apple - iPhone 5s 16GB Cell Phone - Gold (AT&T)",
+      "shortDescription": "4-inch Retina displayA7 chip with M7 motion coprocessorTouch ID fingerprint sensorNew 8MP iSight camera with True Tone flash1080p HD video recordingFaceTime HD camera",
+      "longDescription": "The highly advanced iPhone 5s features the A7 chip with 64-bit architecture, the Touch ID fingerprint sensor, a new 8MP iSight camera, a new FaceTime HD camera, ultrafast LTE wireless, iOS 7, and iCloud. Yet it's as thin and light as ever.",
+      "sku": 1755302,
+      "height": "4.87\"",
+      "width": "2.31\"",
+      "depth": "0.30\"",
+      "weight": "3.95 oz."
+    };
 
     // Determines whether to show a single monster or a list of all
     if (typeof $routeParams.product_id!=='undefined') {
@@ -27,6 +37,7 @@ angular.module('udeHackathon')
       title = 'Product ' + product_id;
       
       $scope.product = products[product_id];
+        $scope.attiphone = attiphone;
     } else {
       $scope.products = products;
     }
